2e3b136460e5d3a58ecc2068ac228eda515464f1
Pausali Sengupta <pausali2004@gmail.com>
2025-05-10 13:05:37 +0530

    Add files via upload

---
+++ backend/Doctors-Appointment/src/main/java/com/webapp/getadoc/model/Patient.java
@@ -0,0 +1,56 @@
+package com.webapp.getadoc.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String contactDetails; // Simple contact info
+
+    @OneToMany(mappedBy = "patient")
+    @JsonManagedReference("patient-appointment") // Changed
+    private List<Appointment> appointments;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactDetails() {
+        return contactDetails;
+    }
+
+    public void setContactDetails(String contactDetails) {
+        this.contactDetails = contactDetails;
+    }
+
+    public List<Appointment> getAppointments() {
+        return appointments;
+    }
+
+    public void setAppointments(List<Appointment> appointments) {
+        this.appointments = appointments;
+    }
+}
